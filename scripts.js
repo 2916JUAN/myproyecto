ead37a66148993a230c0a1b0c56a3677ad206db0
JUANG <juan.angelica2916@gmail.com>
2025-08-21 20:23:01 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// funcion para cerrar el sidenav
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
